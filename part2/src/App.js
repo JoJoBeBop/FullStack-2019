06f7008888cca8823316da7329d724ca3ff499f7
JoJoBeBop <jori.blom@metropolia.fi>
2019-07-09 19:27:23 +0300

    2.9 Done

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,30 +2,61 @@ import React, {useState} from 'react'
 import ReactDOM from "react-dom";
 
 const Person = (person) => {
-    console.log(person);
     return (
-        <p>{person.person.name}</p>
+        <div>
+            <p>{person.person.name}</p>
+            <p>{person.person.number}</p>
+            <hr/>
+        </div>
     )
 };
 
 const App = () => {
 
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas'}
+        {
+            name: 'Arto Hellas',
+            number: "0502566488"
+        }
     ]);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+
 
     //setNewName
     const handleNewName = (event) => {
-        console.log(event.target.value);
+        persons.map((per) => {
+                console.log(per.name);
+
+                if (per.name === event.target.value) {
+                    alert(`${per.name} is already in use pal`)
+                }
+            }
+        );
+
         setNewName(event.target.value)
+    };
+
+    //setNewNumber
+    const handleNewNumber = (event) => {
+        persons.map((per) => {
+                console.log(per.number);
+
+                if (per.number === event.target.value) {
+                    alert(`${per.number} is already in use pal`)
+                }
+            }
+        );
+
+        setNewNumber(event.target.value)
     };
 
     //Adds newName to the list
     const addName = (event) => {
         event.preventDefault();
         const nameObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         };
 
         setPersons(persons.concat(nameObject));
@@ -33,7 +64,8 @@ const App = () => {
     };
 
 
-    const showNames = () => persons.map(person => console.log(person.name) ||
+    //Maps through names and calls Person
+    const showNames = () => persons.map(person => /*console.log(person) ||*/
         <Person
             key={person.name}
             person={person}
@@ -46,14 +78,19 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={addName}>
                 <div>
-                    name:
-                    <br/>
+                    Name:<br/>
                     <input value={newName}
                            onChange={handleNewName}/>
-                    <button type="submit">add</button>
+                </div>
+                <div>
+                    Number:<br/>
+                    <input
+                        value={newNumber}
+                        onChange={handleNewNumber}/>
 
                 </div>
                 <div>
+                    <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
