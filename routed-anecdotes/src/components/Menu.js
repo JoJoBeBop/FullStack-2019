edd686c253ec33bca841fc5a46371bcac6e97459
JoJoBeBop <jori.blom@metropolia.fi>
2019-08-14 15:41:07 +0300

    7.2 Done

---
+++ routed-anecdotes/src/components/Menu.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
+
+const Menu = () => {
+  <div>
+    <p>dicks</p>
+  </div>
+};
+
+export default Menu
